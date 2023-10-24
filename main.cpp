f55a8b08593af01d29a603b9ed1428743d9d6ce3
catarquaza <iraturner15@gmail.com>
2023-10-24 15:12:24 -0700

    dsadasf

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 //Floraburn: Grass Fire Shockfin: Water Electric Frostmoth: Ice Bug Shadowmind: Dark Physic Voltbrawler: Electric Fighting Terrafey: Rock Fairy Ironcladore Rock Steel 
 string Mons[10] = {"Floraburn","Shockfin","Frostmoth", "Shadowmind", "Voltbrawler" , "Terrafey" , "Ironcladore", "Spectrake", "Toxiwarrior", "Metatuff"};
-int * picker()
+int * picker(int seed)
 {
+    srand(seed);
     int mon[3];
     int Mon1 = rand() % 9;
     int Mon2 = rand() % 9;
@@ -50,9 +51,10 @@ int main()
     int * team1;
     int * team2;
     srand(time(0));
-    team1 = picker();
-    srand(time(0));
-    team2 = picker();
+    int seed = rand();
+    team1 = picker(seed);
+    seed = rand();
+    team2 = picker(seed);
     string roster1[3] = {Mons[team1[1]], Mons[team1[2]], Mons[team1[3]]};
     string roster2[3] = {Mons[team2[1]], Mons[team2[2]], Mons[team2[3]]};
     cout << "Team 1: \n";
@@ -61,7 +63,7 @@ int main()
         cout << roster1[i] << "\n";
     }
     cout << "Team 2: \n";
-    for (int i = 0; i < 3; i++)
+     for (int i = 0; i < 3; i++)
     {
         cout << roster2[i] << "\n";
     }
